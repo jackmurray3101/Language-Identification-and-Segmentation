0a8afdb70e1c75f090684f8cb5e6052eccca5298
Jack Murray <jmurray3101@gmail.com>
2022-09-12 11:53:55 +1000

    added training script for gadi

---
+++ stage3/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+#PBS -P wa66
+#PBS -q normal
+#PBS -l ncpus=8
+#PBS -l mem=32GB
+#PBS -l jobfs=40GB
+#PBS -l walltime=00:30:00
+#PBS -l wd
+#PBS -l storage=gdata/wa66
+#PBS -l ngpus=1
+ 
+# Load modules, always specify version number.
+module load python3/3.9.2
+ 
+# Run Python applications
+python3 train.py > $PBS_JOBID.log

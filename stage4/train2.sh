eca65c0e5923d3a87365ad462ba983dfc8679028
Jack Murray <jmurray3101@gmail.com>
2022-09-19 11:06:19 +1000

    num workers comparison with 1 GPU

--- stage4/train2.sh
+++ stage4/train2.sh
@@ -2,13 +2,13 @@
  
 #PBS -P wa66
 #PBS -q gpuvolta
-#PBS -l ncpus=24
+#PBS -l ncpus=12
 #PBS -l mem=16GB
 #PBS -l jobfs=10GB
 #PBS -l walltime=04:00:00
 #PBS -l wd
 #PBS -l storage=gdata/wa66
-#PBS -l ngpus=2
+#PBS -l ngpus=1
 
 # Load modules, always specify version number.
 module load python3/3.9.2

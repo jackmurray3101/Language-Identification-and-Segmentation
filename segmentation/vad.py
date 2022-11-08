fa8f308f83fbacd27e028e6d97e5259bb00aaa23
Jack Murray <jmurray3101@gmail.com>
2022-11-08 23:31:19 +1100

    deleted big wav files

--- segmentation/vad.py
+++
@@ -1,7 +0,0 @@
-from pyannote.audio import Pipeline
-pipeline = Pipeline.from_pretrained("pyannote/voice-activity-detection",
-                                    use_auth_token="ACCESS_TOKEN_GOES_HERE")
-output = pipeline("c:\\Users\\Jack\\Desktop\\Thesis\\code\\segmentation\\cleaned_data\\lan_14\\en-female.wav")
-
-for speech in output.get_timeline().support():
-  print(speech)
